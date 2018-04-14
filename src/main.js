96a73e7c3cfd1ce5a1d9de113c3acb0f8d40e3db
sj <sebastianthegreatful@gmail.com>
2018-04-14 22:53:47 +0200

    If not dev then use hardcoded backend url

--- src/main.js
+++ src/main.js
@@ -4,8 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-// Vue.prototype.$hostname = process.env.NODE_ENV === 'development' ? 'http://' + window.location.hostname + ':8080/api' : 'http://oneliners.fitfit.dk:8080/api'
-Vue.prototype.$backend = 'http://' + window.location.hostname + ':8080/api'
+Vue.prototype.$backend = process.env.NODE_ENV === 'development' ? 'http://' + window.location.hostname + ':8080/api' : 'http://api.oneliners.fitfit.dk/api'
 
 /* eslint-disable no-new */
 new Vue({
